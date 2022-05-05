aa24b5a05706faf384df35415267f1a8f4b8d6ca
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-05-04 21:16:10 -0500

    Agregando componente Playlist

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,11 @@
+function Playlist() {
+  return (
+    <div className="Playlist">
+      <input defaultValue={'New Playlist'} />
+      // <Tracklist />
+      <button className="Playlist-save">SAVE TO SPOTIFY</button>
+    </div>
+  )
+}
+
+export default Playlist
