9a6a5ff085b627cf9bd6aa364b439fafe6e14c45
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-05-11 09:29:23 -0500

    Añadiendo método

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 export default class Track extends React.Component {
 
-  renderAction() {
-    if (isRemoval) {
-      return <button className='Track-action'>-</button>
-    } else {
-      return <button className='Track-action'>+</button>
-    }
+  constructor(props) {
+    super(props)
+
+    // Binding methods
+    this.addTrack = this.addTrack.bind(this);
   }
+
+  // renderAction() {
+  //   if (isRemoval) {
+  //     return <button className='Track-action'>-</button>
+  //   } else {
+  //     return <button className='Track-action'>+</button>
+  //   }
+  // }
 
   addTrack() {
     this.props.track = this.props.addTrack;
@@ -21,8 +28,8 @@ export default class Track extends React.Component {
           <h3>{this.props.track.name}</h3>
           <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <button className="Track-action">
-          {/* <!-- + or - will go here --> */}
+        <button className="Track-action" onClick={this.addTrack}>
+
         </button>
       </div>
     )
