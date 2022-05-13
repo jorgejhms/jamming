0d2330a3ce392a4ce9fe52728c790034aa530144
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-05-13 08:39:05 -0500

    formateando código

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -6,8 +6,11 @@ class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={'New Playlist'} />
-        <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} />
+        <input defaultValue={"New Playlist"} />
+        <TrackList
+          tracks={this.props.playlistTracks}
+          onRemove={this.props.onRemove}
+        />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     )
