5b7195927c92019ba608f6dd3ec35f92137337df
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-05-20 08:59:56 -0500

    eliminando de github información de Spotify API

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,31 @@
+const clientId = '3b97633dbaed4b1caf98057da74bd2a7';
+const redirectUri = 'http://localhost:3000/'
+
 let accessToken;
 
 const Spotify = {
     getAccessToken() {
         if (accessToken) {
             return accessToken;
+        }
+
+        // Implicit Grant Flow
+
+        // check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            // This clears the parameters, allowing us to grab a new access token when it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
         }
     }
 };
