7d264d84dc0de237d435b0ff2ce92adcda0b2abf
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-06-03 08:40:12 -0500

    Convirtiendo SearchResults.js a componentes

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import TrackList from '../TrackList/TrackList'
-import './SearchResults.css'
+import React from "react";
+import TrackList from "../TrackList/TrackList";
+import "./SearchResults.css";
 
-class SearchResults extends React.Component {
-  render() {
-    return (
-      <div className="SearchResults">
-        <h2>Results</h2>
-        <TrackList
-          tracks={this.props.searchResults}
-          onAdd={this.props.onAdd}
-          onRemove={this.props.onRemove}
-          isRemoval={false}
-        />
-      </div>
-    )
-  }
+export default function SearchResults(props) {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList
+        tracks={props.searchResults}
+        onAdd={props.onAdd}
+        onRemove={props.onRemove}
+        isRemoval={false}
+      />
+    </div>
+  );
 }
-
-export default SearchResults
