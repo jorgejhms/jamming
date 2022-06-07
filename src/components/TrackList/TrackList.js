72e5996c24b86d3b3d2fb057ce9b404eaa1f8e01
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-06-07 12:14:08 -0500

    Correcciones

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,23 +1,39 @@
-import React from 'react'
-import Track from '../Track/Track'
-import './TrackList.css'
+import React from "react";
+import Track from "../Track/Track";
+import "./TrackList.css";
 
-class TrackList extends React.Component {
-  render() {
-    return (
-      <div className="TrackList">
-        {this.props.tracks.map(track => (
-          <Track
-            key={track.id}
-            track={track}
-            onAdd={this.props.onAdd}
-            onRemove={this.props.onRemove}
-            isRemoval={this.props.isRemoval}
-          />
-        ))}
-      </div>
-    )
-  }
+export default function TrackList(props) {
+  return (
+    <div className="TrackList">
+      {props.tracks.map((track) => (
+        <Track
+          key={track.id}
+          track={track}
+          onAdd={props.onAdd}
+          onRemove={props.onRemove}
+          isRemoval={props.isRemoval}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default TrackList
+// class TrackList extends React.Component {
+//   render() {
+//     return (
+//       <div className="TrackList">
+//         {this.props.tracks.map(track => (
+//           <Track
+//             key={track.id}
+//             track={track}
+//             onAdd={this.props.onAdd}
+//             onRemove={this.props.onRemove}
+//             isRemoval={this.props.isRemoval}
+//           />
+//         ))}
+//       </div>
+//     )
+//   }
+// }
+
+// export default TrackList
